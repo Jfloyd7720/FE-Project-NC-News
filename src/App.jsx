6748a0a83d74e67441f3c816fe5df9729dca9ce0
Jfloyd7720 <floyd.latc@gmail.com>
2025-02-17 15:18:06 +0000

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { getArticles } from "./server";
+import { Header } from "./Header";
+import { Articles } from "./Article";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    getArticles().then((data) => {
+      console.log(data);
+      setArticles(data);
+    });
+  }, []);
+  if (!articles.length) {
+    return <p>Loading... please wait up to 30 seconds</p>;
+  }
   return (
     <>
+      <Header />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {articles.map((article) => {
+          return (
+            <Articles
+              article_id={article.article_id}
+              article_img={article.article_img_url}
+              author={article.author}
+              topic={article.topic}
+              title={article.title}
+            />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
