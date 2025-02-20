7f5638cea0ed2c2f2bdcc4ba786cb2ab98ca4361
Jfloyd7720 <floyd.latc@gmail.com>
2025-02-20 12:49:51 +0000

    fix rendering and layout of comment section

--- src/CommentSectiom.jsx
+++ src/CommentSectiom.jsx
@@ -4,36 +4,62 @@ import { useEffect } from "react";
 import { newsAPI } from "./server";
 
 export const CommentSection = ({ id }) => {
+  const [comments, setComments] = useState([]);
+  newsAPI.get(`articles/${id.id}/comments`).then((res) => {
+    setComments(res.data);
+  });
   const [tempComment, setTempComment] = useState("");
   const deleteComment = (id) => {
     newsAPI.delete(`comments/${id}`).then((res) => {
-      setTempComment("");
       alert("comment posted successfully");
     });
+    setTempComment("");
+    setComments((prev) => prev.filter((comment) => comment.comment_id != id));
   };
   const postComment = (e) => {
     e.preventDefault();
-    console.log("hello");
     newsAPI
       .post(`articles/${id.id}/comments`, {
         author: "jessjelly",
         body: tempComment,
       })
       .then((res) => {
-        console.log(res);
+        setComments((prev) => {
+          return [
+            ...prev,
+            {
+              author: "jessjelly",
+              body: tempComment,
+              votes: 0,
+              comment_id: res.data.comment_id,
+            },
+          ];
+        });
         alert("comment posted successfully");
+        setTempComment("");
       });
-    setTempComment(" ");
   };
-  const [comments, setComments] = useState([]);
-  useEffect(() => {
-    newsAPI.get(`articles/${id.id}/comments`).then((res) => {
-      setComments(res.data);
-    });
-  }, [tempComment]);
 
   return (
     <div className="comment-section-container">
+      <form className="add-comment" action="">
+        <input
+          type="text"
+          placeholder="Write a commennt here"
+          value={tempComment}
+          onChange={(e) => {
+            setTempComment(e.target.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={(e) => {
+            postComment(e);
+          }}
+        >
+          Add Comment
+        </button>
+      </form>
       {comments.map((comment) => {
         return (
           <div key={comment.comment_id} className="comment">
@@ -55,24 +81,6 @@ export const CommentSection = ({ id }) => {
           </div>
         );
       })}
-      <form className="add-comment" action="">
-        <input
-          type="text"
-          placeholder="Write a commennt here"
-          value={tempComment}
-          onChange={(e) => {
-            setTempComment(e.target.value);
-          }}
-        />
-        <button
-          type="button"
-          onClick={(e) => {
-            postComment(e);
-          }}
-        >
-          Add Comment
-        </button>
-      </form>
     </div>
   );
 };
